b30e2229bc7e344d7e7fbc893eae1b86de1410f5
Kevin Schuetz <schuetzk@gmail.com>
2020-04-26 14:30:51 -0500

    Use scala.util.Random for shuffling

--- src/main/scala/dev/marksman/custombingobuilder/service/Shuffler.scala
+++ src/main/scala/dev/marksman/custombingobuilder/service/Shuffler.scala
@@ -1,6 +1,5 @@
 package dev.marksman.custombingobuilder.service
 
-import dev.marksman.kraftwerk.{Generators, Seed, ValueSupply}
 import zio.{Task, UIO}
 
 import scala.util.Random
@@ -11,27 +10,10 @@ trait Shuffler[A] {
 
 class DefaultShuffler[A](candidates: Vector[A],
                          seed: Long) extends Shuffler[A] {
-  private val supply = buildSupply(candidates, seed)
-  private val iterator = supply.iterator()
+  private val random = new scala.util.Random(seed)
 
-  def nextShuffle: UIO[Vector[A]] = UIO(iterator.next)
-
-  private def buildSupply(candidates: Vector[A],
-                          seed: Long): ValueSupply[Vector[A]] = {
-    import scala.jdk.CollectionConverters._
-    type JVector = dev.marksman.collectionviews.Vector[A]
-    val input = new JVector {
-
-      override def size(): Int = candidates.size
-
-      def unsafeGet(index: Int): A = candidates(index)
-    }
-    Generators.generateShuffled(input).fmap(xs => Vector() ++ xs.asScala)
-      .run(Seed.create(seed))
-  }
-
+  def nextShuffle: UIO[Vector[A]] = UIO(random.shuffle(candidates))
 }
-
 
 object DefaultShuffler {
   def main(args: Array[String]): Unit = {
@@ -46,7 +28,7 @@ object DefaultShuffler {
       _ <- Task(println(xs))
     } yield ()
 
-    (1 to 20).foreach(_ => runtime.unsafeRun(test))
+    (1 to 100).foreach(_ => runtime.unsafeRun(test))
 
   }
 
